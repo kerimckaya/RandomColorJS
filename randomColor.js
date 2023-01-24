10b267ac6e569d2900a7ae128757b4e98a22df34
kerim can kaya <netdiscovernmap@gmail.com>
2023-01-24 14:48:08 +0300

    comments

--- randomColor.js
+++ randomColor.js
@@ -1,11 +1,11 @@
-function GetrandomColorList(numberOfColors) {
-    var ColorArray = new Array();
-        for (var i = 0 ; i < numberOfColors ; i++) {
+function GetrandomColorList(numberOfColors=7) {
+    var ColorArray = new Array(); // create new string array 
+        for (var i = 0 ; i < numberOfColors ; i++) { // A repeating loop was created as many as the desired number of colors
             let colorHextString = "#";
-            for (var j = 0 ; j < 3 ; j++){
-               colorHextString+= ""+parseInt(Math.random() * 256).toString(16);
+            for (var j = 0 ; j < 3 ; j++){ //3 different numbers are derived because the hex colors consist of 3 different hex numbers
+               colorHextString+= ""+parseInt(Math.random() * 256).toString(16); // The generated random number was converted to hex codes and added to the string
             }
-             ColorArray.push(colorHextString)
+             ColorArray.push(colorHextString) // produced color added to list
             }
         console.log(ColorArray);
     }
